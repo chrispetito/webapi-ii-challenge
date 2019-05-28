c27cf74578fcb7f9b657d10343b52a5a37db245d
chrispetito <christopherpetito@gmail.com>
2019-05-28 14:13:52 -0700

    add rest of get requests, post comment request

--- data/router.js
+++ data/router.js
@@ -18,4 +18,20 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params
+    db.findPostComments(id)
+    .then(comments => {
+        res.json({ comments })
+    })
+})
+
+router.post('/', (req, res) => {
+    const { title, contents, created_at, updated_at } = req.body
+    db.insert({ title, contents, created_at, updated_at })
+    .then(response => {
+        res.status(201).json(response)
+    })
+})
+
 module.exports = router;
