6e425f40e0e38e9f4ad34c165cd5436f2cb579b2
chrispetito <christopherpetito@gmail.com>
2019-05-28 14:53:41 -0700

    add error catches and statuses to get requests

--- data/router.js
+++ data/router.js
@@ -1,70 +1,92 @@
-const db = require('./db')
+const db = require("./db");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 //get all posts
-router.get('/', (req, res) => {
-    db
-    .find().then(posts => {
-        res.status(201)
-        .json(posts)
+router.get("/", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(201).json(posts);
     })
-})
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 
 //get individual post
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    db.findById(id)
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
     .then(post => {
-        res.json({ post })
+      post.length > 0
+        ? res.json({ post })
+        : res
+            .status(404)
+            .json({
+              message: "The post with the specified ID does not exist."
+            });
     })
-})
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." });
+    });
+});
 
 //get comments for individual post
-router.get('/:id/comments', (req, res) => {
-    const { id } = req.params
-    db.findPostComments(id)
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  db.findPostComments(id)
     .then(comments => {
-        res.json({ comments })
+      comments.length > 0
+        ? res.json({ comments })
+        : res
+            .status(404)
+            .json({
+              message: "The post with the specified ID does not exist."
+            });
     })
-})
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
+    });
+});
 
 //new post
-router.post('/', (req, res) => {
-    const { title, contents, created_at, updated_at } = req.body
-    db.insert({ title, contents, created_at, updated_at })
-    .then(response => {
-        res.status(201).json(response)
-    })
-})
+router.post("/", (req, res) => {
+  const { title, contents, created_at, updated_at } = req.body;
+  db.insert({ title, contents, created_at, updated_at }).then(response => {
+    res.status(201).json(response);
+  });
+});
 
 //new comment
-router.post('/:id/comments', (req, res) => {
-    const { id } = req.params;
-    const { text, post_id, created_at, updated_at } = req.body
-    db.insertComment({ text, post_id, created_at, updated_at })
-    .then(response => {
-        res.json(response)
-    })
-})
+router.post("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  const { text, post_id, created_at, updated_at } = req.body;
+  db.insertComment({ text, post_id, created_at, updated_at }).then(response => {
+    res.json(response);
+  });
+});
 
 //delete individual post
-router.delete('/:id', (req, res) => {
-    const { id } = req.params
-    db.remove(id)
-    .then(response => {
-        res.json(response)
-    })
-})
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id).then(response => {
+    res.json(response);
+  });
+});
 
 //modify individual post
-router.put('/:id', (req, res) => {
-    const { id } = req.params
-    const { text, post_id, created_at, updated_at } = req.body
-    db.update(id, req.body)
-    .then(response => {
-        res.json(response)
-    })
-})
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { text, post_id, created_at, updated_at } = req.body;
+  db.update(id, req.body).then(response => {
+    res.json(response);
+  });
+});
 
 module.exports = router;
